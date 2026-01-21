190f15cead94ecfb5823f821683a15f15aa276cd
Arandelle <topaguintsarandell@gmail.com>
2026-01-21 15:07:15 +0800

    feat: add the menu section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import MenuSection from "@/components/MenuSection";
 
 export default function Home() {
   return <>
     <Hero />
+    <MenuSection />
     <Footer />
   </>
 
