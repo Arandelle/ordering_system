9f79d4bdd6254a05504eb859bef0d1326514f19e
Arandelle <topaguintsarandell@gmail.com>
2026-01-27 12:46:40 +0800

    create: reusable order now button

---
+++ components/ui/OrderNowButton.tsx
@@ -0,0 +1,22 @@
+import { useCart } from "@/contexts/CartContext";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const OrderNowButton = () => {
+  const router = useRouter();
+  const { setIsCartOpen } = useCart();
+  const navigateToOrder = () => {
+    router.push("/menu");
+    setIsCartOpen(false);
+  };
+  return (
+    <button
+      onClick={navigateToOrder}
+      className="px-4 py-2 mt-4 rounded-xl bg-[#e13e00] hover:bg-[#c13500] text-white transition-colors shadow-xl cursor-pointer"
+    >
+      Order Now!
+    </button>
+  );
+};
+
+export default OrderNowButton;
