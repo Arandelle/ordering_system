0d9b316119521397b60d366a580396355ee0b170
Arandelle <topaguintsarandell@gmail.com>
2026-01-27 18:38:29 +0800

    create styled skeleton for suspense fallback

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,8 +1,9 @@
 import React, { Suspense } from "react";
 import FullMenuSection from "./FullMenuSection";
+import MenuSectionSkeleton from "@/components/ui/MenuSectionSkeleton";
 
 const page = () => {
-  return <Suspense fallback={<div>Loading menu...</div>}>
+  return <Suspense fallback={<MenuSectionSkeleton />}>
     <FullMenuSection />
   </Suspense>;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/homepage/Footer";
 import Hero from "@/components/homepage/Hero";
 import MenuSection from "@/components/homepage/MenuSection";
+import MenuSectionSkeleton from "@/components/ui/MenuSectionSkeleton";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Suspense fallback={<div>Loading menu...</div>}>
+      <Suspense fallback={<MenuSectionSkeleton />}>
         <MenuSection variant="landing" />
       </Suspense>
       <Footer />
