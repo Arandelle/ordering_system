c2475f2a0bf5940aea7d9b26398cf87541edf9a4
Arandelle <topaguintsarandell@gmail.com>
2026-01-27 17:41:11 +0800

    feat: added not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+// app/not-found.tsx
+"use client";
+
+import { CircleAlert } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center space-y-4 bg-gray-100">
+      <Image
+        height={250}
+        width={250}
+        src={"/images/harrison_logo.png"}
+        alt="harrison logo"
+      />
+      <div className="flex items-center gap-4">
+        <CircleAlert size={40} />
+        <h3 className="text-2xl font-bold text-center text-slate-900">
+          Page not found
+        </h3>
+      </div>
+      <p className="text-center text-slate-500">
+        Looks like the page you're looking for doesn't exist.
+      </p>
+      <Link
+        href="/"
+        className="mt-4 px-4 py-2 bg-[#e13e00] text-white rounded hover:bg-[#c13500] transition"
+      >
+        Go to Homepage
+      </Link>
+    </div>
+  );
+}
