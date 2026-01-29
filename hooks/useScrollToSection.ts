086758394b78dd816411800ab3a48b985ffdb3d7
Arandelle <topaguintsarandell@gmail.com>
2026-01-29 18:54:54 +0800

    feat: create scrollToSection hook for those section who has scrolling based on params move the constant align on root

---
+++ hooks/useScrollToSection.ts
@@ -0,0 +1,15 @@
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export function useScrollToSection(){
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const target = searchParams.get("scroll");
+
+        if(!target) return;
+
+        const el = document.getElementById(`${target}-section`);
+        el?.scrollIntoView({behavior: "smooth"});
+    }, [searchParams])
+}
