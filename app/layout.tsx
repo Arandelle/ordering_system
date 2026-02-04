2d007d712dc623c2c9250b84af7dbec56243af58
Arandelle <topaguintsarandell@gmail.com>
2026-02-04 15:33:38 +0800

    feat: add order provider to the root to support the modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { OrderProvider } from "@/contexts/OrderContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,9 +32,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster richColors position="top-right" closeButton />
-        {children}
-        {modal}
+        <OrderProvider>
+          <Toaster richColors position="top-right" closeButton />
+          {children}
+          {modal}
+        </OrderProvider>
       </body>
     </html>
   );
