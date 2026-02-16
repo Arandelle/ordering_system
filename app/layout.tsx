53a06253919efb53c5aeab27d6f468e0d4895311
Arandelle <topaguintsarandell@gmail.com>
2026-02-16 18:40:25 +0800

    feat: use the tanstack provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { OrderProvider } from "@/contexts/OrderContext";
+import { QueryProvider } from "@/provider/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,11 +70,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <OrderProvider>
-          <Toaster richColors position="top-right" closeButton />
-          {children}
-          {modal}
-        </OrderProvider>
+        <QueryProvider>
+          <OrderProvider>
+            <Toaster richColors position="top-right" closeButton />
+            {children}
+            {modal}
+          </OrderProvider>
+        </QueryProvider>
       </body>
     </html>
   );
