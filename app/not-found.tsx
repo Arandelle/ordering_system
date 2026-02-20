7270b33fe8341d161387625ec737bb0dc45f35eb
Arandelle <topaguintsarandell@gmail.com>
2026-02-20 12:17:00 +0800

    feat: revert to actual not found instead of maintenance

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 // app/not-found.tsx
 "use client";
 
-import { CircleAlert, MailWarning, Telescope } from "lucide-react";
+import { CircleAlert } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,35 +9,30 @@ export default function NotFound() {
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-4 bg-gray-100">
       <Image
-        // height={250}
-        // width={250}
-         height={300}
-        width={300}
-        src={"/images/maintenance.png"}
+        height={250}
+        width={250}
+        src={"/images/harrison_logo.png"}
         alt="harrison logo"
       />
-      <div className="flex items-center gap-4">
-        {/* <CircleAlert size={40} /> */}
-        <Telescope size={40}/>
-        <h3 className="text-2xl font-bold text-center text-slate-900">
-          {/* Page not found */}
-          Page is under maintenance
+      <div className="flex items-center gap-4 text-[#e13e00]">
+        <CircleAlert size={40} />
+        <h3 className="text-2xl font-bold text-center">
+          Page not found
         </h3>
       </div>
       <p className="text-center text-slate-500">
-        {/* Looks like the page you're looking for doesn't exist. */}
-      Looks like the page you're looking is under mainternance
+        Looks like the page you're looking for doesn't exist.
       </p>
       <div className="flex gap-4">
         <Link
           href="/"
-          className="mt-4 px-4 py-2 border border-[#e13e00] text-[#e13e00] rounded hover:bg-[#c13500] transition"
+          className="mt-4 px-4 py-2 border border-[#e13e00] text-[#e13e00] rounded hover:bg-[#c13500] hover:text-white transition-colors"
         >
           Go to Homepage
         </Link>
         <button
           onClick={() => window.history.back()}
-          className="mt-4 px-4 py-2 bg-[#e13e00] text-white rounded hover:bg-[#c13500] transition"
+          className="mt-4 px-4 py-2 bg-[#e13e00] text-white rounded hover:bg-[#c13500] transition-colors cursor-pointer"
         >
           Go Back
         </button>
