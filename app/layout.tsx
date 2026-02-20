9e3ff4682455471f489d081edfa00c13371684a5
Arandelle <topaguintsarandell@gmail.com>
2026-02-20 12:15:43 +0800

    feat: remove the order context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { OrderProvider } from "@/contexts/OrderContext";
 import { QueryProvider } from "@/provider/QueryProvider";
 
 const geistSans = Geist({
@@ -19,34 +18,44 @@ export const metadata: Metadata = {
   title: "Home | Harrison House of Inasal & BBQ",
   description:
     "Discover Harrison: proudly Filipino BBQ and Inasal, served with stories and barkada vibes. New branches, online orders, and collabs coming soon—follow the grill!",
-  keywords: ["filipino bbq", "inasal", "bbq", "restaurant", "makati", "food", "order"],
-  authors: [{name: "Harrison Inasal & BBQ"}],
+  keywords: [
+    "filipino bbq",
+    "inasal",
+    "bbq",
+    "restaurant",
+    "makati",
+    "food",
+    "order",
+  ],
+  authors: [{ name: "Harrison Inasal & BBQ" }],
   creator: "Harrison Inasal & BBQ",
-  metadataBase : new URL("https://eris-admin.com"),
+  metadataBase: new URL("https://eris-admin.com"),
 
   openGraph: {
     title: "Harrison Inasal & BBQ",
     description: "Filipino Inasal and BBQ",
     url: "https://eris-admin.com",
     siteName: "Harrison Inasal & BBQ",
-    images: [{
-      url: "/images/mission-product-img.jpg",
-      width: 1200,
-      height: 630
-    }],
-    type: "website"
+    images: [
+      {
+        url: "/images/mission-product-img.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
   },
 
   twitter: {
     card: "summary_large_image",
     title: "Harrison Inasal & BBQ",
     description: "Filipino Inasal and BBQ",
-    images: ["/images/harrison_logo.png"]
+    images: ["/images/harrison_logo.png"],
   },
 
   robots: {
     index: false, // change to true once domain is final
-    follow: false // change to true once domain is final
+    follow: false, // change to true once domain is final
   },
 
   icons: {
@@ -71,11 +80,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          <OrderProvider>
-            <Toaster richColors position="top-right" closeButton />
-            {children}
-            {modal}
-          </OrderProvider>
+          <Toaster richColors position="top-right" closeButton />
+          {children}
+          {modal}
         </QueryProvider>
       </body>
     </html>
