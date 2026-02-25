030effae053b3a5b1879026ac49afb05e3eb563a
Arandelle <topaguintsarandell@gmail.com>
2026-02-25 16:44:42 +0800

    feat: aggregation for top products and sales data

--- lib/dashboardStats.ts
+++ lib/dashboardStats.ts
@@ -1,5 +1,6 @@
 import { Order } from "@/models/Orders";
 import { connectDB } from "./mongodb";
+import { SalesData, TopProduct } from "@/types/adminType";
 
 export async function getDashboardStats() {
   await connectDB();
@@ -33,4 +34,62 @@ export async function getDashboardStats() {
     bestSellingProduct: bestSellerResult[0]?._id || "NA",
     bestSellingCount: bestSellerResult[0]?.totalSold || 0,
   };
+}
+
+export async function getSalesData():Promise<SalesData[]> {
+  await connectDB();
+  const sevenDaysAgo = new Date();
+
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  const result = await Order.aggregate([
+    {
+      $match: {
+        createdAt: {$gt: sevenDaysAgo},
+        status: "completed"
+      },
+    },
+    {
+      $group: {
+        _id: {
+          $dateToString: {format: "%m/%d", date: "$createdAt"}
+        },
+        sales: {$sum: "$total.total"}
+      }
+    },
+    {
+      $sort: { _id: 1}
+    },
+    {
+      $project: {_id: 0, date: "$_id", sales: 1}
+    }
+  ]);
+
+  return result;
+}
+
+export async function getTopProducts(): Promise<TopProduct[]>{
+  
+  await connectDB();
+
+  const result = await Order.aggregate([
+    {
+      $match: {
+        status: "completed"
+      }
+    },
+    {
+      $unwind: "$items"
+    },
+    {
+      $group: {
+        _id: "$items.name",
+        sale: {$sum: "$items.quantity"}
+      }
+    },
+    {$sort : {sales : -1}},
+    {$limit: 5},
+    {$project: {_id: 0, name: "$_id", sales: 1}}
+  ])
+  return result
 }
