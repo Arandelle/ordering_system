544f3ee4e72b73d1d0accaf3c5fdcc46e5060e6e
Arandelle <topaguintsarandell@gmail.com>
2026-03-02 15:49:45 +0800

    update some buttons

--- components/admin/OrderActionButton.tsx
+++ components/admin/OrderActionButton.tsx
@@ -26,7 +26,7 @@ return (
   <button
     onClick={handleClick}
     disabled={isPending}
-    className={`font-bold text-white py-2 px-2.5 cursor-pointer ${transition.variant}`}
+    className={`text-sm rounded-full font-bold text-white py-2 px-4 cursor-pointer ${transition.variant}`}
   >
     {isPending ? "Updating..." : transition.label}
 
