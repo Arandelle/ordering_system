8583bd4a82bad560442022f46f5c10bafbf4f8cc
Arandelle <topaguintsarandell@gmail.com>
2026-03-04 11:37:53 +0800

    feat: create patch endpoint

---
+++ app/api/branch/[id]/route.ts
@@ -0,0 +1,45 @@
+import { connectDB } from "@/lib/mongodb";
+import { Branch } from "@/models/Branch";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDB();
+    const { id } = await context.params;
+    if (!id) {
+      return NextResponse.json(
+        {
+          error: "Branch id is required!",
+        },
+        { status: 400 },
+      );
+    }
+
+    const branch = await Branch.findById(id);
+
+    if (!branch) {
+      return NextResponse.json({ error: "Branch not found." }, { status: 404 });
+    }
+
+    branch.isActive = !branch.isActive;
+    await branch.save();
+
+    return NextResponse.json(branch);
+    
+  } catch (error) {
+    console.error("PATCH /api/branch/[id] error:", error);
+
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error
+            ? error.message
+            : "Something went wrong while updating the branch status.",
+      },
+      { status: 500 },
+    );
+  }
+}
