4c5de0ae865ac4bbe1a08f545b101b339ff5836d
Arandelle <topaguintsarandell@gmail.com>
2026-03-07 12:15:49 +0800

    feat: metadata for manifest

--- app/customer/layout.tsx
+++ app/customer/layout.tsx
@@ -3,6 +3,13 @@ import Footer from "@/components/ui/Footer";
 import Header from "@/components/customer/homepage/Header";
 import { CartProvider } from "@/contexts/CartContext";
 import React from "react";
+import PWAInstallPrompt from "@/components/PWAInstallPrompt";
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: "Customer | House of Inasal & BBQ",
+  manifest: "/manifest.json",
+}
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,6 +19,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
         <CartDrawer />
         {children}
         <Footer />
+        <PWAInstallPrompt/> 
       </CartProvider>
     </>
   );
