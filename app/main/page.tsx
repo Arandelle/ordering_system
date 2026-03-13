c62bfdf19625b10b480a1a2371ab0444cd8c161b
Arandelle <topaguintsarandell@gmail.com>
2026-03-13 17:02:33 +0800

    feat: add the events page to main page

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,6 +2,7 @@ import HeroVideo from "@/components/main/HeroBanner";
 import MainLocationSection from "@/components/main/MainLocationSection";
 import MissionVision from "@/components/main/MissionVision";
 import ProductMain from "@/components/main/ProductMain";
+import EventsPage from "../customer/events/page";
 
 const MainPage = () => {
   return (
@@ -10,6 +11,9 @@ const MainPage = () => {
       <ProductMain />
       <MissionVision />
       <MainLocationSection /> {/** Includes order now CTA */}
+      <div className="pt-1 bg-gray-500">
+        <EventsPage />
+      </div>
     </>
   );
 };
