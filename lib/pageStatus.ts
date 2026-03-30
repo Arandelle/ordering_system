64cf6e0d6ee31e858b3ca0368fb1ac38b3a0fae3
Arandelle <topaguintsarandell@gmail.com>
2026-03-30 14:44:56 +0800

    feat: list of route that should be blocked to access for the meantime in live

---
+++ lib/pageStatus.ts
@@ -0,0 +1,18 @@
+export const pageStatus = {
+    '/checkout' : false,
+    '/orders': false,
+}
+
+export function isRouteBlocked(pathname: string): boolean {
+  if (pathname in pageStatus) {
+    return !pageStatus[pathname as keyof typeof pageStatus]
+  }
+
+  for (const route in pageStatus) {
+    if (pathname.startsWith(route + '/')) {
+      return !pageStatus[route as keyof typeof pageStatus]
+    }
+  }
+
+  return false
+}
