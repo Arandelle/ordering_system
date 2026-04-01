cce358068d275110669a168a01fc80fa3f4a5f2c
Arandelle <topaguintsarandell@gmail.com>
2026-04-01 11:25:45 +0800

    feat: scalable and reusable auth, for admin and customer

--- lib/getAuth.ts
+++ lib/getAuth.ts
@@ -1,23 +1,66 @@
-import { cookies } from "next/headers";
-import { verifyToken } from "./verifyToken";
 import { StaffRole } from "@/types/staff";
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function getAuthAdmin() {
-  const token = (await cookies()).get("admin_token")?.value;
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in env variables!");
+}
+
+export const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export const COOKIE_NAMES = {
+  ADMIN_TOKEN: "admin_token",
+  CUSTOMER_TOKEN: "customer_token",
+};
+
+export type CookieType = (typeof COOKIE_NAMES)[keyof typeof COOKIE_NAMES];
+
+export async function verifyToken(token: string) {
+  try {
+    const { payload } = await jwtVerify(token, JWT_SECRET);
+    return payload;
+  } catch (error) {
+    console.error("JWT verification failed:", error);
+    return null;
+  }
+}
+
+export async function getAuth(request: NextRequest, cookieName: CookieType) {
+  const token = request.cookies.get(cookieName)?.value;
 
   if (!token) return null;
 
-  try {
-    const payload = await verifyToken(token);
+  const payload = await verifyToken(token);
+  if (!payload) return null;
 
-    if (!payload) return null;
+  return payload;
+}
 
-    return {
-      id: payload.id as string,
-      email: payload.email as string,
-      role: payload.role as StaffRole,
-    };
-  } catch (error) {
-    return null;
-  }
+export async function getAdminAuth(request: NextRequest) {
+  const payload = await getAuth(request, COOKIE_NAMES.ADMIN_TOKEN);
+  if (!payload) return null;
+
+  return {
+    id: payload.id as string,
+    email: payload.email as string,
+    role: payload.role as StaffRole,
+    branch: payload.branch as string,
+  };
+}
+
+export async function getCustomerAuth(request: NextRequest) {
+  const payload = await getAuth(request, COOKIE_NAMES.CUSTOMER_TOKEN);
+  if (!payload) return null;
+
+  return {
+    id: payload.id as string,
+    email: payload.email as string,
+  };
+}
+
+export async function requireAdmin(request: NextRequest) {
+  const admin = await getAdminAuth(request);
+  if (!admin) throw new Error("Unauthorized!");
+
+  return admin;
 }
