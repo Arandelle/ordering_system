b51afcedd2fb6d33625f309fbfc7411db4f1921b
Arandelle <topaguintsarandell@gmail.com>
2026-04-14 15:45:02 +0800

    feat: add position

--- models/SubCategory.ts
+++ models/SubCategory.ts
@@ -4,8 +4,10 @@ const SubCategorySchema = new Schema(
   {
     name: { type: String, required: true },
     category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    position: { type: Number, default: 1 },
   },
   { timestamps: true },
 );
 
-export const SubCategory = models.SubCategory || mongoose.model("SubCategory", SubCategorySchema);
+export const SubCategory =
+  models.SubCategory || mongoose.model("SubCategory", SubCategorySchema);
