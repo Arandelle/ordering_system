77f997bd02a05f63bccd6cc102072109d6e9caf0
Arandelle <topaguintsarandell@gmail.com>
2026-04-18 16:30:48 +0800

    feat: separate computing of form errors

---
+++ app/customer/checkout/useFormErrors.ts
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { CustomerSchema, OrderFormSchema, OrderFormState, ShippingSchema } from "./FormSchema";
+import z from "zod";
+
+export type CustomerErrors = Partial<Record<keyof z.infer<typeof CustomerSchema>, string>>;
+export type ShippingErrors = Partial<Record<keyof z.infer<typeof ShippingSchema>, string>>;
+
+const useFormErrors = (orderDetails: OrderFormState) => {
+  const [customerErrors, setCustomerErrors] = useState<CustomerErrors>({});
+  const [shippingErrors, setShippingErrors] = useState<ShippingErrors>({});
+
+  const validateAll = (): boolean => {
+    const result = OrderFormSchema.safeParse(orderDetails);
+
+    if (result.success) {
+      setCustomerErrors({});
+      setShippingErrors({});
+      return true;
+    }
+
+    const formattedCustomerErrors: CustomerErrors = {};
+    const formattedShippingErrors: ShippingErrors = {};
+
+    result.error.issues.forEach((err) => {
+      const path = err.path.join(".");
+
+      switch (path) {
+        case "customer.firstname":
+          formattedCustomerErrors.firstname = err.message;
+          break;
+        case "customer.lastname":
+          formattedCustomerErrors.lastname = err.message;
+          break;
+        case "customer.customerPhone":
+          formattedCustomerErrors.customerPhone = err.message;
+          break;
+        case "customer.customerEmail":
+          formattedCustomerErrors.customerEmail = err.message;
+          break;
+        case "shippingAddress.line1":
+          formattedShippingErrors.line1 = err.message;
+          break;
+        case "shippingAddress.city":
+          formattedShippingErrors.city = err.message;
+          break;
+        case "shippingAddress.province":
+          formattedShippingErrors.province = err.message;
+          break;
+        case "shippingAddress.postalCode":
+          formattedShippingErrors.postalCode = err.message;
+          break;
+      }
+    });
+
+    setCustomerErrors(formattedCustomerErrors);
+    setShippingErrors(formattedShippingErrors);
+    return false;
+  };
+
+  return { validateAll, customerErrors, shippingErrors, setCustomerErrors, setShippingErrors };
+};
+
+export default useFormErrors;
