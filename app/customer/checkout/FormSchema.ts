afbade096768d472b5abe4c0c359efc394d6b723
Arandelle <topaguintsarandell@gmail.com>
2026-04-18 16:31:29 +0800

    feat: better form types using z schema

---
+++ app/customer/checkout/FormSchema.ts
@@ -0,0 +1,35 @@
+// ─── Schema ───────────────────────────────────────────────────────────────────
+
+import z from "zod";
+
+export const CustomerSchema = z.object({
+  firstname: z.string().min(1, "Firstname is required"),
+  lastname: z.string().min(1, "Last name is required"),
+
+  customerPhone: z
+    .string()
+    .regex(/^(09|\+639)\d{9}$/, "Invalid PH mobile number").optional().or(z.literal("")),
+
+  customerEmail: z
+    .string()
+    .email("Invalid emaild address"),
+
+  note: z.string().optional().or(z.literal("")),
+});
+
+export const ShippingSchema = z.object({
+  line1: z.string().min(1, "Address is required"),
+  line2: z.string().optional(),
+  city: z.string().min(1, "City is required"),
+  province: z.string().min(1, "Province is required"),
+  postalCode: z.string().min(1, "Postal code is required"),
+  country: z.literal("Philippines"),
+  landmark: z.string().optional(),
+});
+
+export const OrderFormSchema = z.object({
+  customer: CustomerSchema,
+  shippingAddress: ShippingSchema,
+});
+
+export type OrderFormState = z.infer<typeof OrderFormSchema>;
