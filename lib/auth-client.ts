c5563bcefffb85c3398f00cc91d74352aa72fca1
Arandelle <topaguintsarandell@gmail.com>
2026-04-23 12:55:17 +0800

    feat: plugins for additional fields -firstName and lastName

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,12 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "@/lib/auth"; // your auth.ts
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL, // Ensure this is set in your .env
+  baseURL: process.env.NEXT_PUBLIC_URL,
+  plugins: [
+    inferAdditionalFields<typeof auth>(), // ← infers firstName, lastName from your auth config
+  ],
 });
+
+export type Session = typeof authClient.$Infer.Session;
