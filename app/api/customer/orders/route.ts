cb8254ab0ea40189a660ea291cac424a82b89fe8
Arandelle <topaguintsarandell@gmail.com>
2026-05-11 14:36:43 +0800

    feat: better parse of request

--- app/api/customer/orders/route.ts
+++ app/api/customer/orders/route.ts
@@ -2,24 +2,42 @@ import { connectDB } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import { requireBetterAuth } from "@/lib/getAuth";
 import { queryOrders } from "@/lib/orders/orderService";
+import { parseRequestQuery } from "@/lib/query-helpers";
 
 export async function GET(request: NextRequest) {
   try {
     await connectDB();
     const customer = await requireBetterAuth();
 
+    const { page, limit, skip, sort, match } = parseRequestQuery(request, {
+      exactFields: ["status"],
+      searchFields: [
+        "paymentInfo.customerName",
+        "paymentInfo.customerEmail",
+        "paymentInfo.customerPhone",
+        "status",
+        "paymentInfo.referenceNumber",
+      ],
+      defaultLimit: 20,
+      maxLimit: 50,
+      defaultSort: { status: 1, createdAt: -1 },
+    });
+
+    const filter: Record<string, any> = { ...match, customerId: customer?._id };
+
     // ============================================
     // QUERY PARAMETERS
     // ============================================
-    const searchParams = request.nextUrl.searchParams;
+
     const result = await queryOrders({
-      filter: { customerId: customer._id }, // customer can ONLY see their own
-      page: parseInt(searchParams.get("page") || "1"),
-      limit: parseInt(searchParams.get("limit") || "20"),
+      filter,
+      page,
+      limit,
+      skip,
+      sort,
     });
 
     return NextResponse.json(result);
-
   } catch (error: any) {
     if (error.message === "Unauthorized!") {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
