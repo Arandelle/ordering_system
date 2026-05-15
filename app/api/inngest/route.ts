80293e0c361afc391eec4720bd8948d1c777cc3d
Arandelle <topaguintsarandell@gmail.com>
2026-05-15 14:54:03 +0800

    feat: add event type

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,8 +2,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { myScheduledJob } from "@/inngest/functions/myScheduledJob";
+import { expireOrder } from "@/inngest/functions/expireOrder";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [myScheduledJob],
+  functions: [expireOrder], // put functions here
 });
