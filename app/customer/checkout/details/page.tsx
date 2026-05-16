c384f496c97aa535d7f39a28f75cb320f0f8fd26
Arandelle <topaguintsarandell@gmail.com>
2026-05-16 18:55:50 +0800

    feat: pass the validateField functions on customer details page as onblur call

--- app/customer/checkout/details/page.tsx
+++ app/customer/checkout/details/page.tsx
@@ -1,16 +1,18 @@
-'use client'
+"use client";
 
 import React from "react";
 import CustomerDetails from "./CustomerDetails";
 import { useCheckout } from "@/contexts/CheckoutContext";
 
 const page = () => {
-  const { orderDetails, customerErrors, handleStateChange } = useCheckout();
+  const { orderDetails, customerErrors, handleStateChange, validateField } =
+    useCheckout();
   return (
     <CustomerDetails
       customerData={orderDetails.customer}
       errors={customerErrors}
       onChange={handleStateChange}
+      onBlur={(field, value) => validateField("customer", field, value)}
     />
   );
 };
