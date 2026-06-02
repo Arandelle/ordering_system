2807bd16f62947b6895e842cb798e423971fa9ec
Arandelle <topaguintsarandell@gmail.com>
2026-06-02 15:21:01 +0800

    feat: add validity rule

--- types/voucher.types.ts
+++ types/voucher.types.ts
@@ -3,11 +3,19 @@ export type VoucherUsageRule = {
   isConsumable: boolean;
 };
 
+export type VoucherValidityUnit = "minute" | "hour" | "day" | "month" | "year";
+
+export type VoucherValidityRule = {
+  duration: number;
+  unit: VoucherValidityUnit;
+};
+
 export type VoucherRule = {
   enabled: boolean;
   voucherAmount: number;
   minimumPurchase: number;
   usageRule: VoucherUsageRule;
+  validityRule: VoucherValidityRule;
 };
 
 export const DEFAULT_VOUCHER_USAGE_RULE: VoucherUsageRule = {
@@ -15,11 +23,25 @@ export const DEFAULT_VOUCHER_USAGE_RULE: VoucherUsageRule = {
   isConsumable: false,
 };
 
+export const VOUCHER_VALIDITY_UNITS: VoucherValidityUnit[] = [
+  "minute",
+  "hour",
+  "day",
+  "month",
+  "year",
+];
+
+export const DEFAULT_VOUCHER_VALIDITY_RULE: VoucherValidityRule = {
+  duration: 1,
+  unit: "year",
+};
+
 export const DEFAULT_VOUCHER_RULE: VoucherRule = {
   enabled: false,
   voucherAmount: 1000,
   minimumPurchase: 3000,
   usageRule: DEFAULT_VOUCHER_USAGE_RULE,
+  validityRule: DEFAULT_VOUCHER_VALIDITY_RULE,
 };
 
 export const VOUCHER_STATUSES = [
@@ -31,6 +53,8 @@ export const VOUCHER_STATUSES = [
 
 export const VOUCHER_SOURCE_TYPES = [
   "promo_card",
+  "birthday",
+  "campaign",
   "order_refund",
   "manual",
 ] as const;
