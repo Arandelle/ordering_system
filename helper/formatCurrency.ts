ddfca16248285c9f0eb26ec88d6e508fdcc274d6
Arandelle <topaguintsarandell@gmail.com>
2026-06-04 16:07:49 +0800

    feat: add fallback value

--- helper/formatCurrency.ts
+++ helper/formatCurrency.ts
@@ -1,4 +1,7 @@
 export function formatCurrency(value: number) {
+
+  if(!value) return '--'
+
   return `₱${value.toLocaleString("en-PH", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
