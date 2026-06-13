49957f67564a9b4a26273c33e29db6ac764f186b
Arandelle <topaguintsarandell@gmail.com>
2026-06-13 18:59:55 +0800

    feat: create validation for 1 object id

--- helper/getValidObjectIds.ts
+++ helper/getValidObjectIds.ts
@@ -1,7 +1,17 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 export function getValidObjectIds(ids: string[] | undefined) {
   if (!Array.isArray(ids)) return [];
 
   return [...new Set(ids)].filter((id) => mongoose.Types.ObjectId.isValid(id));
 }
+
+export const getValidObjectId = (id: unknown) => {
+  const normalizeId = id?.toString();
+
+  if (!normalizeId || !Types.ObjectId.isValid(normalizeId)) {
+    return null;
+  }
+
+  return new Types.ObjectId(normalizeId);
+};
