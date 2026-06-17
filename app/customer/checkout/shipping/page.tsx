ff6ccc30cac9ad785b8ed1a563605a6408a1aefc
Arandelle <topaguintsarandell@gmail.com>
2026-06-17 14:56:43 +0800

    refactor: add onblur for error message and handlecoordinates

--- app/customer/checkout/shipping/page.tsx
+++ app/customer/checkout/shipping/page.tsx
@@ -5,8 +5,25 @@ import ShippingAddress from "./ShippingAddress";
 import { useCheckout } from "@/contexts/CheckoutContext";
 
 const page = () => {
-  const { orderDetails, shippingErrors, openModal, handleStateChange } =
-    useCheckout();
+  const {
+    orderDetails,
+    shippingErrors,
+    openModal,
+    handleStateChange,
+    handleShippingCoordinatesChange,
+    validateField,
+  } = useCheckout();
+
+  const handleShippingBlur = (field: string, value: string) => {
+    validateField("shippingAddress", field, value);
+  };
+
+  const handleCoordinatesChange = (
+    coordinates: typeof orderDetails.shippingAddress.coordinates,
+  ) => {
+    handleShippingCoordinatesChange(coordinates);
+    validateField("shippingAddress", "coordinates", coordinates);
+  };
     
   return (
     <ShippingAddress
@@ -14,6 +31,8 @@ const page = () => {
       errors={shippingErrors}
       openModal={openModal}
       onChange={handleStateChange}
+      onBlur={handleShippingBlur}
+      onCoordinatesChange={handleCoordinatesChange}
     />
   );
 };
