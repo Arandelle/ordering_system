62c70f338b4e097ba89dfd39a58d43cabf3975bb
Arandelle <topaguintsarandell@gmail.com>
2026-06-23 14:57:01 +0800

    feat: filter inventories based on selected branch

--- hooks/api/useBranchInventory.ts
+++ hooks/api/useBranchInventory.ts
@@ -2,11 +2,18 @@ import { InventoryItem } from "@/types/inventory_types";
 import { apiClient } from "@/lib/apiClient";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { useAdminBranchContext } from "@/contexts/AdminBranchContext";
+import { getBranchQuery } from "@/helper/getBranchQuery";
 
 export const useBranchInventories = () => {
+  const { selectedBranchId } = useAdminBranchContext();
+
   return useQuery({
-    queryKey: ["inventory_sync"],
-    queryFn: () => apiClient.get<InventoryItem[]>("/staff/inventories"),
+    queryKey: ["inventory_sync", selectedBranchId],
+    queryFn: () =>
+      apiClient.get<InventoryItem[]>(
+        `/staff/inventories${getBranchQuery(selectedBranchId)}`,
+      ),
   });
 };
 
@@ -26,6 +33,7 @@ interface UpdateInventoryResponse {
 
 export const useUpdateInventory = () => {
   const queryClient = useQueryClient();
+  const { selectedBranchId } = useAdminBranchContext();
 
   return useMutation<
     UpdateInventoryResponse,
@@ -38,10 +46,15 @@ export const useUpdateInventory = () => {
     }: {
       id: string;
       payload: UpdateInventoryPayload;
-    }) => apiClient.put(`/staff/inventories/${id}`, payload),
+    }) =>
+      apiClient.put(
+        `/staff/inventories/${id}${getBranchQuery(selectedBranchId)}`,
+        payload,
+      ),
 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["inventory_sync"] });
+      queryClient.invalidateQueries({ queryKey: [selectedBranchId] });
     },
     onError: (error) => {
       toast.error(error.message);
@@ -62,12 +75,17 @@ interface BulkUpdateResponse {
 
 export const useBulkUpdateInventory = () => {
   const queryClient = useQueryClient();
+  const { selectedBranchId } = useAdminBranchContext();
 
   return useMutation<BulkUpdateResponse, Error, BulkUpdateItem[]>({
     mutationFn: (items: BulkUpdateItem[]) =>
-      apiClient.put(`/staff/inventories/bulk`, { items }),
+      apiClient.put(
+        `/staff/inventories/bulk${getBranchQuery(selectedBranchId)}`,
+        { items },
+      ),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["inventory_sync"] });
+      queryClient.invalidateQueries({ queryKey: [selectedBranchId] });
     },
   });
 };
