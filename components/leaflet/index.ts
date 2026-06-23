5117b112d0922806b2245d0f4ea4bed4838c20b7
Arandelle <topaguintsarandell@gmail.com>
2026-06-23 18:19:01 +0800

    refactor: declare the mappolygon

--- components/leaflet/index.ts
+++ components/leaflet/index.ts
@@ -3,5 +3,6 @@ export { default as DraggableMapMarker } from "./DraggableMapMarker";
 export { default as MapCircle } from "./MapCircle";
 export { default as MapClickHandler } from "./MapClickHandler";
 export { default as MapMarker } from "./MapMarker";
+export { default as MapPolygon } from "./MapPolygon";
 export { default as RecenterMap } from "./RecenterMap";
 export type { MapCoordinates, MapPosition } from "./types";
