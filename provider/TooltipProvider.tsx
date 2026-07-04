6f39a5419599d9edccc0b31f5a7821fdf5a683d3
Arandelle <topaguintsarandell@gmail.com>
2026-07-04 17:32:09 +0800

    feat: client tooltip provider

---
+++ provider/TooltipProvider.tsx
@@ -0,0 +1,8 @@
+// components/providers/TooltipProvider.tsx
+"use client";
+
+import { Tooltip } from "react-tooltip";
+
+export default function TooltipProvider() {
+  return <Tooltip id="app-tooltip" place="top" style={{ zIndex: 9999, }} />;
+}
