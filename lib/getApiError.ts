d59bce9e3bdb921a84a9bac08036b9fe6d515361
Arandelle <topaguintsarandell@gmail.com>
2026-07-07 12:07:27 +0800

    refactor: handle error type string

--- lib/getApiError.ts
+++ lib/getApiError.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from "next/server";
 /**
- * 
+ *
  * @param error - Error message you want to tell to the user
  * @param statusCode - status code of it e.g - 401, 404, 500
  * @param fallbackMessage - incase error message failed
- * @returns 
+ * @returns
  */
 export function getAPIError(
   error: unknown,
   statusCode: number = 500,
   fallbackMessage = "Internal Server Error",
 ) {
+  const err = typeof error === "string" ? new Error(error) : error;
   console.error(error);
 
-  const message = error instanceof Error ? error.message : fallbackMessage;
+  const message = err instanceof Error ? err.message : fallbackMessage;
   return NextResponse.json({ error: message }, { status: statusCode });
 }
